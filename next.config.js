401053d70a73d4d13f4799b2e9998830028a9965
Arsen-go <68658606+Arsen-go@users.noreply.github.com>
2024-01-14 17:10:30 +0400

    FIX origin

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ const nextConfig = {
         // matching all API routes
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Credentials", value: "false" },
           { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
           {
             key: "Access-Control-Allow-Methods",
